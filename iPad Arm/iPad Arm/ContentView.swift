fa9a71c1dc0a2fc14a89ebc38aad3412be505b2b
Logan <lrfoster03@gmail.com>
2024-06-12 20:35:27 -0700

    Created iPad Application

---
+++ iPad Arm/iPad Arm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iPad Arm
+//
+//  Created by Logan Foster on 6/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iPad Arm/iPad Arm/iPad_ArmApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPad_ArmApp.swift
+//  iPad Arm
+//
+//  Created by Logan Foster on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct iPad_ArmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
